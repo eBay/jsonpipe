fb4bb4ad2f212b7cb3d696d27d8eb641d41cca14
spadmanabhan <spadmanabhan@ebay.com>
2015-02-11 12:05:42 -0800

    Adding watch and test task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	mochaPhantomJS = require('gulp-mocha-phantomjs');
 
+// Build task, to generate the bundled browserify file
 gulp.task('build', function(){
 	gulp.src('lib/jsonpipe.js')
 		.pipe(browserify({
@@ -8,3 +10,19 @@ gulp.task('build', function(){
 		}))
 		.pipe(gulp.dest('.'));
 });
+
+// The watch task
+gulp.task('watch', function() {
+	gulp.watch('lib/**/*.js', ['build']);	
+});
+
+// Add the test task
+gulp.task('test', ['build'], function() {
+	return gulp.src('test/jsonpipe.html')
+				.pipe(mochaPhantomJS());
+});
+
+// Make the default task as test
+gulp.task('default', ['test']);
+
+
