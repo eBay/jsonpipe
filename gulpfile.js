00f8810d4e484ec8536ae114dff189b85876a99c
spadmanabhan <spadmanabhan@ebay.com>
2015-12-22 08:37:32 -0800

    Integrating with coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
+'use strict';
+
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
-    mochaPhantomJS = require('gulp-mocha-phantomjs');
+    replace = require('gulp-replace'),
+    mochaPhantomJS = require('gulp-mocha-phantomjs'),
+    istanbul = require('gulp-istanbul'),
+    istanbulReport = require('gulp-istanbul-report'),
+    coveralls = require('gulp-coveralls');
 
 // Build task, to generate the bundled browserify file
 gulp.task('build', function() {
-    gulp.src('lib/jsonpipe.js')
+    return gulp.src('lib/jsonpipe.js')
         .pipe(browserify({
             standalone: "jsonpipe"
         }))
@@ -16,10 +22,50 @@ gulp.task('watch', function() {
     gulp.watch('lib/**/*.js', ['build']);
 });
 
-// Add the test task
 gulp.task('test', ['build'], function() {
     return gulp.src('test/jsonpipe.html')
         .pipe(mochaPhantomJS());
+});
+
+// Add a task to instrument src files
+gulp.task('instrument', ['build'], function() {
+    return gulp.src('./jsonpipe.js')
+        .pipe(istanbul({coverageVariable: "__coverage__"}))
+        .pipe(gulp.dest('lib-cov/'));
+});
+
+// Add the test coverage task task
+gulp.task('test-cov', ['instrument'], function() {
+    var htmlFile = 'test/jsonpipe.html',
+        replaceStrs = ['../jsonpipe.js', '../lib-cov/jsonpipe.js'],
+        coverageJSON = 'coverage/coverage.json';
+
+    return gulp.src(htmlFile)
+        .pipe(replace.apply(null, replaceStrs))
+        .pipe(gulp.dest('test')) // Override the same file
+        .pipe(mochaPhantomJS({
+            reporter: 'spec',
+            phantomjs: {
+                hooks: 'mocha-phantomjs-istanbul',
+                coverageFile: coverageJSON
+            }
+        }))
+        // generate coverage.json after finish
+        .on('finish', function() {
+            return gulp.src(coverageJSON)
+                .pipe(istanbulReport({reporters: ['lcov']}));
+        })
+        // Revert the html file
+        .on('finish', function() {
+            return gulp.src(htmlFile)
+                .pipe(replace.apply(null, replaceStrs.reverse()))
+                .pipe(gulp.dest('test'));
+        });
+});
+
+gulp.task('test-coveralls', ['test-cov'], function() {
+    return gulp.src('./coverage/lcov.info')
+        .pipe(coveralls());
 });
 
 // Make the default task as test
